7d0edb67ce2af33204f565aa5ed8d264b5a4dc2b
spc <spacewe@aliyun.com>
2017-04-07 20:26:42 +0800

    Add files via upload

---
+++ 按键实验/key.h
@@ -0,0 +1,20 @@
+#ifndef	_key_H
+#define _key_H
+#include "stm32f10x.h"
+#define K_UP GPIO_Pin_0
+#define K_DOWN GPIO_Pin_3
+#define K_RIGHT GPIO_Pin_4
+#define K_LEFT GPIO_Pin_2
+
+#define k_up GPIO_ReadInputDataBit(GPIOA,K_UP)		 //获取按键的状态
+#define k_down GPIO_ReadInputDataBit(GPIOE,K_DOWN)
+#define k_right GPIO_ReadInputDataBit(GPIOE,K_RIGHT)	   //不能加 ;
+#define k_left GPIO_ReadInputDataBit(GPIOE,K_LEFT)
+		
+void key_init(void);
+void delay_us(u32 i);
+void delay_ms(u32 i);		
+
+
+#endif
+
